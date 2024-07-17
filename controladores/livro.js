0fb8e59d2a7b05a8c0a6d90515e52bfb1f9ca303
Fernando Ellery <fernando.hle@gmail.com>
2024-07-16 23:49:01 -0300

    Lidando com parameters

--- controladores/livro.js
+++ controladores/livro.js
@@ -1,8 +1,9 @@
-const { getTodosLivros } = require("./servicos/livros")
+const { getTodosLivros, getLivrosPorId } = require("../servicos/livro")
+
 
 function getLivros(req, res) {
     try {
-        const livros = getTodosLivros
+        const livros = getTodosLivros()
         res.send(livros)
     } catch (error) {
         res.status(500)
@@ -10,6 +11,18 @@ function getLivros(req, res) {
     }
 }
 
+function getLivro(req, res) {
+    try {
+        const id = req.params.id
+        const livro = getLivrosPorId(id)
+        res.send(livro)
+    } catch (error) {
+        res.status(500)
+        res.send(error.message)
+    }
+}
+
 module.exports = {
-    getLivros
+    getLivros,
+    getLivro
 }
